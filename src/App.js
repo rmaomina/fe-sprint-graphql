8e6f39fb0ed1afc93f7f474c35590baf6c935ac8
mina <rmaomina@gmail.com>
2022-10-09 02:44:47 +0900

    install @octokit/graphql

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import { useEffect, useState } from "react";
+import { graphql } from "@octokit/graphql";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [repository, setRepository] = useState({});
+  const [viewer, setViewer] = useState({});
+
+	async function getRepository() {
+		const GHP_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+		const { repository, viewer } = await graphql({
+      headers: {
+				authorization: `Bearer ${GHP_TOKEN}`,
+			},
+      owner: "codestates-seb",
+      name: "agora-states-fe",
+      num: 5,
+      query: `query repository($owner: String!, $name: String!, $num: Int!) {
+        repository(name: $name, owner: $owner) {
+          discussions(first: $num) {
+            edges {
+              node {
+                category {
+                  name
+                }
+                author {
+                  login
+                  avatarUrl
+                }
+                createdAt
+                title
+                id
+                url
+                answer {
+                  author {
+                    login
+                    avatarUrl
+                  }
+                  bodyHTML
+                  createdAt
+                  id
+                }
+              }
+            }
+          }
+        }
+        viewer {
+          login
+          avatarUrl
+        }
+      }`,
+    },);
+		return {repository, viewer};
+	}
+
+	useEffect(() => {
+		getRepository()
+			.then((data) => {
+				setRepository(data.repository);
+				setViewer(data.viewer);
+			})
+			.catch((error) => {
+				console.log(Error, error);
+			});
+	}, []);
+
+	console.log(repository);
+	console.log(viewer);
+
+	return (
+		<div className="App">
+			<h1>Hello, Agora States</h1>
+		</div>
+	);
 }
 
 export default App;
